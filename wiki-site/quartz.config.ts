5ece1c6bdb864f30cdb76dd809d37ebfb08d42eb
Rafael129 <walkklaw@gmail.com>
2026-04-17 16:51:18 +0800

    fix: remove Latex plugin to suppress KaTeX unicode warnings
    Chinese text containing $ signs was being parsed as LaTeX math mode,
    causing noise in build logs.

--- wiki-site/quartz.config.ts
+++ wiki-site/quartz.config.ts
@@ -66,7 +66,6 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: 'shortest' }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: 'katex' }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
